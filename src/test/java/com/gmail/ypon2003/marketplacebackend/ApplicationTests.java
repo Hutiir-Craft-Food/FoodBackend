4fc5bfd6d1c3ef21e797d079c86a99ad53be4bc5
Katrusya <ruda.venera@gmail.com>
2024-07-08 15:14:27 +0300

    updated Application and ApplicationTests

--- src/test/java/com/gmail/ypon2003/marketplacebackend/ApplicationTests.java
+++ src/test/java/com/gmail/ypon2003/marketplacebackend/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketPlaceBackendApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
