f647f14b816b734b3325485ca70f6c051e19ddeb
Katrusya <ruda.venera@gmail.com>
2024-07-09 18:36:02 +0300

    №18 SellerRepository was created/changed

---
+++ src/main/java/com/gmail/ypon2003/marketplacebackend/repositories/SellerRepository.java
@@ -0,0 +1,7 @@
+package com.gmail.ypon2003.marketplacebackend.repositories;
+
+import com.gmail.ypon2003.marketplacebackend.models.SellerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<SellerEntity, Long> {
+}
