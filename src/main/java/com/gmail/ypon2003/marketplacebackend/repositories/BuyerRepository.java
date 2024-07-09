72fe0812a909be8671869300caddb28ddd06e8da
Katrusya <ruda.venera@gmail.com>
2024-07-09 18:32:04 +0300

    №9 BuyerRepository was created

---
+++ src/main/java/com/gmail/ypon2003/marketplacebackend/repositories/BuyerRepository.java
@@ -0,0 +1,7 @@
+package com.gmail.ypon2003.marketplacebackend.repositories;
+
+import com.gmail.ypon2003.marketplacebackend.models.BuyerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyerRepository extends JpaRepository<BuyerEntity, Long> {
+}
