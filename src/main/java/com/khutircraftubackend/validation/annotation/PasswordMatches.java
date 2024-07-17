cd5c4c58121d05eaf3fba05d484ec40cc26d221a
Yrii <caravell@ukr.net>
2024-07-17 23:22:24 +0300

    SCRUM-8: create registration Seller

---
+++ src/main/java/com/khutircraftubackend/validation/annotation/PasswordMatches.java
@@ -0,0 +1,19 @@
+package com.khutircraftubackend.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = com.khutircraftubackend.validation.validator.PasswordMatchesValidator.class)
+public @interface PasswordMatches {
+
+    String message() default "Пароль і підтвердження не співпадають";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
