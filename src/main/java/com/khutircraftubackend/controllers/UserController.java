d0bb0f0e45aa0546538fc5464f5e1766967a958d
Yrii <caravell@ukr.net>
2024-07-23 23:16:18 +0300

    SCRUM-21: add methods authenticateUser(), updatePassword(), recoverPassword

--- src/main/java/com/khutircraftubackend/controllers/UserController.java
+++ src/main/java/com/khutircraftubackend/controllers/UserController.java
@@ -1,23 +1,81 @@
 package com.khutircraftubackend.controllers;
 
 import com.khutircraftubackend.dto.UserDTO;
+import com.khutircraftubackend.dto.security.JwtResponse;
+import com.khutircraftubackend.dto.security.LoginRequest;
+import com.khutircraftubackend.dto.security.PasswordRecoveryRequest;
+import com.khutircraftubackend.dto.security.PasswordUpdateRequest;
+import com.khutircraftubackend.security.JwtUtils;
+import com.khutircraftubackend.services.PersonDetailsServices;
 import com.khutircraftubackend.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Клас UserController обробляє запити, пов'язані з користувачами.
+ */
+
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final PersonDetailsServices personDetailsServices;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+
+        if(authentication != null) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String jwt = jwtUtils.generateJwtToken(userDetails.getUsername());
+
+            return ResponseEntity.ok(new JwtResponse(jwt));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private Authentication authenticate(String email, String password) {
+        UserDetails userDetails = personDetailsServices.loadUserByUsername(email);
+
+        if(userDetails != null && passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        } else {
+            return null;
+        }
+    }
 
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO) {
         UserDTO registeredUser = userService.registerNewUser(userDTO);
         return ResponseEntity.ok(registeredUser);
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<Void> updatePassword(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) {
+
+        boolean isUpdated = userService.updatePassword(passwordUpdateRequest);
+        if(isUpdated) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/recovery")
+    public ResponseEntity<Void> recoverPassword(@Valid @RequestBody PasswordRecoveryRequest passwordRecoveryRequest) {
+        userService.initiatePasswordRecovery(passwordRecoveryRequest.getEmail());
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/confirm")
--- src/main/java/com/khutircraftubackend/services/UserService.java
+++ src/main/java/com/khutircraftubackend/services/UserService.java
@@ -1,17 +1,28 @@
 package com.khutircraftubackend.services;
 
+import com.khutircraftubackend.dto.security.PasswordUpdateRequest;
 import com.khutircraftubackend.dto.UserDTO;
 import com.khutircraftubackend.mapper.UserMapper;
 import com.khutircraftubackend.models.Role;
 import com.khutircraftubackend.models.User;
 import com.khutircraftubackend.repositories.UserRepository;
+import com.khutircraftubackend.security.JwtUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+/**
+ * Клас UserService реалізує бізнес-логіку для роботи з користувачами.
+ * <p>
+ * Цей клас містить методи для створення, оновлення, видалення та отримання користувачів.
+ * </p>
+ */
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +37,7 @@ public class UserService {
         User user = UserMapper.INSTANCE.userDTOToUser(userDTO);
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole(Role.SELLER);
+        user.setRole(userDTO.role() == Role.SELLER ? Role.SELLER : Role.BUYER);
         user.setEnabled(false);//до підтвердження електронною поштою
         User savedUser = userRepository.save(user);// надіслати сюди логіку електронного листа з підтвердженням
         return UserMapper.INSTANCE.userToUserDTO(savedUser);
