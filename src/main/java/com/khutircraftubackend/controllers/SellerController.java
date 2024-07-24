87c631aa613091b80eab18fba4d38bacd91d747f
Yrii <caravell@ukr.net>
2024-07-25 01:12:34 +0300

    SCRUM-21: delete registerSeller(), confirmSeller() and add createSeller() with for changes in logic

--- src/main/java/com/khutircraftubackend/controllers/SellerController.java
+++ src/main/java/com/khutircraftubackend/controllers/SellerController.java
@@ -4,9 +4,15 @@ import com.khutircraftubackend.dto.SellerDTO;
 import com.khutircraftubackend.services.SellerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Клас SellerController обробляє запити, пов'язані з продавцями.
+ */
 
 @RestController
 @RequestMapping("/v1/seller")
@@ -15,21 +21,10 @@ public class SellerController {
 
     private final SellerService sellerService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerSeller(@Valid @RequestBody SellerDTO sellerDTO) {
-        if(!sellerDTO.isPasswordMatching()) {
-            return new ResponseEntity<>("Пароль та підтвердження паролю не співпадають", HttpStatus.BAD_REQUEST);
-        }
-
-        sellerService.registerSeller(sellerDTO);
-        return new ResponseEntity<>("Продавець зареєстрований успішно", HttpStatus.CREATED);
+    @PostMapping
+    ResponseEntity<SellerDTO> createSeller(@Valid @RequestBody SellerDTO sellerDTO) {
+        SellerDTO createdSeller = sellerService.saveSeller(sellerDTO);
+        return ResponseEntity.ok(createdSeller);
     }
 
-    public ResponseEntity<String> confirmSeller(@RequestParam("code")
-                                                String confirmationCode) {
-        if(sellerService.confirmSeller(confirmationCode)) {
-            return ResponseEntity.ok("Обліковий запис підтверджено.");
-        }
-        return ResponseEntity.badRequest().body("Невірний код підтвердження або обліковий запис уже підтверджено.");
-    }
 }
