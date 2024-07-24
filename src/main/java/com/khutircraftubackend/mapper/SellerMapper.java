d313e416a11627f10974658d238e085c329da277
Yrii <caravell@ukr.net>
2024-07-25 01:14:09 +0300

    SCRUM-21: fix name

--- src/main/java/com/khutircraftubackend/mapper/SellerMapper.java
+++ src/main/java/com/khutircraftubackend/mapper/SellerMapper.java
@@ -1,15 +1,19 @@
 package com.khutircraftubackend.mapper;
 
 import com.khutircraftubackend.dto.SellerDTO;
-import com.khutircraftubackend.models.SellerEntity;
+import com.khutircraftubackend.models.Seller;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+/**
+ * Інтерфейс SellerMapper мапить дані між моделлю Seller та DTO SellerDTO.
+ */
 
 @Mapper
 public interface SellerMapper {
     SellerMapper INSTANCE = Mappers.getMapper(SellerMapper.class);
 
-    SellerEntity toSellerEntity(SellerDTO sellerDTO);
+    Seller SellerDTOToSeller(SellerDTO sellerDTO);
 
-    SellerDTO toSellerDTO(SellerEntity sellerEntity);
+    SellerDTO SellerToSellerDTO(Seller seller);
 }
