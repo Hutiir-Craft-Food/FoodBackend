9dfc6480d40babdff9fbb3ec1505136cc861a7d5
Yrii <caravell@ukr.net>
2024-07-25 01:13:34 +0300

    SCRUM-21: fix with for changes in logic

--- src/main/java/com/khutircraftubackend/dto/SellerDTO.java
+++ src/main/java/com/khutircraftubackend/dto/SellerDTO.java
@@ -1,10 +1,18 @@
 package com.khutircraftubackend.dto;
 
+import com.khutircraftubackend.models.User;
 import com.khutircraftubackend.validation.annotation.PasswordMatches;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
+
+/**
+ * Клас SellerDTO використовується для передачі даних продавця.
+ * <p>
+ * Цей клас містить основні дані користувача, такі як назва компанії, email, номер телефону, податковий номер тощо.
+ * </p>
+ */
 
 @Builder
 @PasswordMatches
@@ -12,7 +20,7 @@ public record SellerDTO(
         @NotBlank(message = "Назва компанії не може бути порожнім")
         @Size(max = 200, message = "Назва компанії не може перевищувати 200 символів")
         @Pattern(regexp = "^[a-zA-Zа-яА-ЯіїєґІЇЄҐ\\s]*$", message = "Назва компанії має містити лише українські або англійські літери")
-        String name,
+        String company,
 
         @NotBlank(message = "E-mail не може бути порожнім")
         @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
@@ -22,22 +30,9 @@ public record SellerDTO(
         @Pattern(regexp = "^\\+380\\d{9}$", message = "Номер телефону має бути у форматі +380__________")
         String phoneNumber,
 
-        @NotBlank(message = "Пароль не може бути порожнім")
-        @Size(min = 8, max = 30, message = "Пароль має містити від 8 до 30 символів")
-        @Pattern(regexp = "^[^!@#$%^&*()_+=]*$", message = "Пароль не може містити спеціальні символи !@#$%^&*()_+=")
-        String password,
+        User user,
 
-        @NotBlank(message = "Підтвердження паролю не може бути порожнім")
-        @Size(min = 8, max = 30, message = "Підтвердження паролю має містити від 8 до 30 символів")
-        @Pattern(regexp = "^[^!@#$%^&*()_+=]*$", message = "Підтвердження паролю не може містити спеціальні символи !@#$%^&*()_+=")
-        String confirmPassword,
+        String tax_code
 
-        String confirmationCode,
-
-        boolean enabled
 ) {
-    //метод перевірки відповідності пароля і підтвердження
-    public boolean isPasswordMatching() {
-        return this.password.equals(this.confirmPassword);
-    }
 }
