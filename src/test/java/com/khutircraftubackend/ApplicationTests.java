52c15de7c426935f421cd792fca37c2d0bb63c11
P.F. <pavel.filkovskiy@gmail.com>
2024-07-30 22:40:12 +0100

    SCRUM-33: rename main class

--- src/test/java/com/khutircraftubackend/ApplicationTests.java
+++ src/test/java/com/khutircraftubackend/ApplicationTests.java
@@ -3,6 +3,6 @@ package com.khutircraftubackend;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketPlaceBackendApplicationTests {
+class ApplicationTests {
 
 }
