0e3d393cf144849259fe2ae74f459ed63659d127
Yrii <caravell@ukr.net>
2024-08-04 14:08:01 +0300

    SCRUM-21: created GlobalExceptionHandler for try catch @Valid exception

--- src/main/java/com/khutircraftubackend/globalException/GlobalExceptionHandler.java
+++ src/main/java/com/khutircraftubackend/globalException/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
-package com.khutircraftubackend.controllers;
+package com.khutircraftubackend.globalException;
 
-import com.khutircraftubackend.security.exceptionJwt.*;
+import com.khutircraftubackend.globalException.exception.*;
 import jakarta.persistence.EntityNotFoundException;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
@@ -77,6 +77,28 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmptyJwtClaimsException.class)
     public ResponseEntity<String> handleEmptyJwtClaimsException(EmptyJwtClaimsException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Handles InvalidTokenException and returns a response with status 401 (Unauthorized).
+     *
+     * @param e the exception to handle
+     * @return the response entity with the exception message and status 401
+     */
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<String> handleInvalidTokenException(InvalidTokenException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * Handles UserNotFoundException and returns a response with status 404 (Not Found).
+     *
+     * @param e the exception to handle
+     * @return the response entity with the exception message and status 404
+     */
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RuntimeException.class)
