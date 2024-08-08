509c156e0c33da5b634ff886d44aabe10bde4298
Yrii <caravell@ukr.net>
2024-08-08 22:22:12 +0100

    SCRUM-8: add Seller

---
+++ src/main/java/com/khutircraftubackend/seller/SellerController.java
@@ -0,0 +1,30 @@
+package com.khutircraftubackend.seller;
+
+import com.khutircraftubackend.seller.SellerDTO;
+import com.khutircraftubackend.seller.SellerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Клас SellerController обробляє запити, пов'язані з продавцями.
+ */
+
+@RestController
+@RequestMapping("/v1/seller")
+@RequiredArgsConstructor
+public class SellerController {
+
+    private final SellerService sellerService;
+
+    @PostMapping
+    ResponseEntity<SellerDTO> createSeller(@Valid @RequestBody SellerDTO sellerDTO) {
+        SellerDTO createdSeller = sellerService.saveSeller(sellerDTO);
+        return ResponseEntity.ok(createdSeller);
+    }
+
+}
---
+++ src/main/java/com/khutircraftubackend/seller/SellerMapper.java
@@ -0,0 +1,17 @@
+package com.khutircraftubackend.seller;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Інтерфейс SellerMapper мапить дані між моделлю Seller та DTO SellerDTO.
+ */
+
+@Mapper
+public interface SellerMapper {
+    SellerMapper INSTANCE = Mappers.getMapper(SellerMapper.class);
+
+    Seller SellerDTOToSeller(SellerDTO sellerDTO);
+
+    SellerDTO SellerToSellerDTO(Seller seller);
+}
