8ec1b3a68bb42d88845357185404124b939263ee
Yrii <caravell@ukr.net>
2024-08-09 00:32:22 +0300

    SCRUM-21:optimisation

--- src/main/java/com/khutircraftubackend/config/SecurityConfig.java
+++ src/main/java/com/khutircraftubackend/config/SecurityConfig.java
@@ -1,42 +1,53 @@
 package com.khutircraftubackend.config;
 
+import com.khutircraftubackend.auth.UserDetailsServicesImpl;
+import com.khutircraftubackend.jwtToken.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/**
- * Клас SecurityConfig відповідає за налаштування безпеки додатку за допомогою Spring Security.
- */
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableAsync
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
 
+    private final UserDetailsServicesImpl userDetailsServiceImpl;
+
+    public final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/v1/user/login",
+                                "/sellers/**",
+                                "/products/**",
                                 "/v1/user/register",
+                                "/v1/user/login",
                                 "/error")
                         .permitAll()
-                        .requestMatchers("/products/**", "/sellers/**").permitAll()// Доступ для всіх
                         .requestMatchers("/seller/**").hasRole("SELLER")
                         .requestMatchers("/buyer/**").hasRole("BUYER")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
@@ -45,19 +56,10 @@ public class SecurityConfig {
                         .loginProcessingUrl("/process_login")
                         .defaultSuccessUrl("/index", true)
                         .failureUrl("/login?error"))
-                .rememberMe(rememberMe -> rememberMe.userDetailsService(userDetailsService))
+                .rememberMe(rememberMe -> rememberMe.userDetailsService(userDetailsServiceImpl))
                 .logout(logout -> logout.logoutUrl("/logout")
-                        .logoutSuccessUrl("/login").permitAll());
-
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                        .logoutSuccessUrl("/login").permitAll())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
     }
 }
