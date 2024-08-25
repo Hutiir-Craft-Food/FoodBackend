ab904afb608fa86c3214f83a498815b032119d40
Ne4upara <ne4upara84@gmail.com>
2024-08-25 11:47:18 +0300

    SCRUM 45 : Add FlyWay

---
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       email VARCHAR(40) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(50) NOT NULL DEFAULT 'USER',
+                       enabled BOOLEAN DEFAULT FALSE,
+                       confirmation_token VARCHAR(255),
+                       creation_date TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_email ON users (email);
