cc06928c1d593dcf1009216c94c94fe4bcaf694b
Yrii <caravell@ukr.net>
2024-10-02 14:22:09 +0300

    SCRUM-69: fix migration, Category and Tests

--- src/main/java/com/khutircraftubackend/category/exception/category/CategoryExceptionMessages.java
+++ src/main/java/com/khutircraftubackend/category/exception/category/CategoryExceptionMessages.java
@@ -7,5 +7,4 @@ public class CategoryExceptionMessages{
     public static final String CATEGORY_NOT_FOUND = "Category not found";
     public static final String CATEGORY_HAS_SUBCATEGORIES_OR_PRODUCTS =
             "Category has subcategories or products, can't be deleted";
-    public static final String CATEGORY_HAS_PRODUCTS = "Category has products";
 }
