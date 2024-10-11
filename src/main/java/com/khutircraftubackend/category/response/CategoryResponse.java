4560e21a729886ec262fa607c81a73ea6ce4bfb6
Yrii <caravell@ukr.net>
2024-10-11 22:58:48 +0300

    SCRUM-69: refactor CategoryService, CategoryMapper, CategoryController, CategoryResponse

--- src/main/java/com/khutircraftubackend/category/response/CategoryResponse.java
+++ src/main/java/com/khutircraftubackend/category/response/CategoryResponse.java
@@ -3,10 +3,11 @@ package com.khutircraftubackend.category.response;
 import lombok.Builder;
 
 @Builder
-public record CategoryResponse (
-        String name,
-        String description,
-        Long parentId,
-        String iconUrl
+public record CategoryResponse(
+		Long id,
+		String name,
+		String description,
+		Long parentId,
+		String iconUrl
 ) {
 }
