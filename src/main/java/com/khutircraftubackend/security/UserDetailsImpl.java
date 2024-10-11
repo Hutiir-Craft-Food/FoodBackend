053e26e0fdc0c8dbc9638a18d977888507b3a956
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2024-10-11 09:31:27 +0300

    SCRUM: 71: (#41)
    * SCRUM-71: fix: corrected bug in UserDetailsConfig
    
    * SCRUM-71: refactor build to new object instantiation for UserDetailsImpl and remove log.info(...) and remove build method from UserDetailsImpl

--- src/main/java/com/khutircraftubackend/security/UserDetailsImpl.java
+++ src/main/java/com/khutircraftubackend/security/UserDetailsImpl.java
@@ -17,46 +17,42 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Getter
 public class UserDetailsImpl implements UserDetails {
-
-    private final UserEntity user;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return this.user.getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    // Статичний метод для створення екземпляра UserDetailsImpl
-    public static UserDetailsImpl build(UserEntity user) {
-        return new UserDetailsImpl(user);
-    }
+	
+	private final UserEntity user;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+	}
+	
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+	
+	@Override
+	public String getUsername() {
+		return this.user.getEmail();
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
 }
