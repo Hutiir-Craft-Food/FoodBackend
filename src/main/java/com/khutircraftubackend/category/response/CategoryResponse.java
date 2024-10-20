b5fb71af3d2f3fb5ae76d96c20cf8a3a4e767d03
Yrii <caravell@ukr.net>
2024-10-20 14:16:32 +0300

    SCRUM-69: Refactor SellerResponse, ProductResponse, CategoryResponse, and ProductMapper to exclude sensitive personal data from responses. Removed unnecessary @NotBlank annotations in ProductUpdateRequest for PATCH updates to allow partial updates of fields.

--- src/main/java/com/khutircraftubackend/category/response/CategoryResponse.java
+++ src/main/java/com/khutircraftubackend/category/response/CategoryResponse.java
@@ -1,8 +1,10 @@
 package com.khutircraftubackend.category.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CategoryResponse(
 		Long id,
 		String name,
--- src/main/java/com/khutircraftubackend/product/response/ProductResponse.java
+++ src/main/java/com/khutircraftubackend/product/response/ProductResponse.java
@@ -1,10 +1,12 @@
 package com.khutircraftubackend.product.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.khutircraftubackend.category.response.CategoryResponse;
 import com.khutircraftubackend.seller.SellerResponse;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ProductResponse(
 		Long id,
         String name,
