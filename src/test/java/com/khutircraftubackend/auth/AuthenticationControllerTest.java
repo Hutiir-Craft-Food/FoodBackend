0b8a898e39034a1f2cc924d6df0b6ae185233c75
Yrii <caravell@ukr.net>
2025-03-20 10:17:43 +0200

    SCRUM-135 Refactor test AuthenticationControllerTest.java

--- src/test/java/com/khutircraftubackend/auth/AuthenticationControllerTest.java
+++ src/test/java/com/khutircraftubackend/auth/AuthenticationControllerTest.java
@@ -36,116 +36,123 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles({"local", "test"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {"spring.main.allow-bean-definition-overriding=true"})
-@Import(JwtTestConfig.class)class AuthenticationControllerTest {
-
+@Import(JwtTestConfig.class)
+class AuthenticationControllerTest {
+    
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @LocalServerPort
     private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Autowired
     private MockMvc mockMvc;
-    
     @Autowired
     private Algorithm algorithm;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
     
     @Nested
     class WrongTokenTests {
-    
+        
         @Test
         @DisplayName("should fail on expired token")
-        void fail_when_authenticating_with_expired_token () {
-        
+        void fail_when_authenticating_with_expired_token() {
+            
             String email = "seller@example.com";
+            
             String expiredToken = JWT.create()
                     .withSubject(email)
                     .withIssuedAt(new Date(System.currentTimeMillis() - 1000))
                     .withExpiresAt(new Date(System.currentTimeMillis() - 100))
                     .sign(algorithm);
-        
+            
             HttpHeaders headers = new HttpHeaders();
             headers.add("Authorization", "Bearer " + expiredToken);
             HttpEntity<String> request = new HttpEntity<String>(headers);
-        
+            
             int offset = 0, limit = 4;
-        
+            
             GlobalErrorResponse errorResponse = restTemplate.exchange(
                     "http://localhost:{port}/v1/products?offset={offset}&limit={limit}",
                     HttpMethod.GET, request, GlobalErrorResponse.class,
                     port, offset, limit).getBody();
-        
+    
+            assertNotNull(errorResponse, "Expected an error response but got null");
             assertEquals(401, errorResponse.status());
             assertTrue(errorResponse.message().startsWith("The Token has expired"));
             assertEquals("/v1/products", errorResponse.path());
         }
-
+        
         @Test
         @DisplayName("should fail when someone forges token's signature")
-        void fail_when_authenticating_with_token_having_wrong_signature () {
-            // TODO:
-            //  programmatically generate token with valid claims but wrong signature:
-            String forgedToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9." +
-                    "eyJzdWIiOiJibGFoLXVzZXIiLCJleHAiOjE3NDAzNTM2MjIsImlhdCI6MTc0MDM1MzU2Mn0." +
-                    "gXCdsHdrNsK80dIwYAzIiC78uIteujERUTdJWHNTRnMhh_-rKn7BlTn3x1fxwXVhHah2Sc5zySJwBx6ADmW_Ww";
-
+        void fail_when_authenticating_with_token_having_wrong_signature() {
+    
+            String email = "seller@example.com";
+            
+            Algorithm wrongAlgorithm = Algorithm.HMAC512("secret");
+            
+            String forgedToken = JWT.create()
+                    .withSubject(email)
+                    .withIssuedAt(new Date(System.currentTimeMillis()))
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 1000))
+                    .sign(wrongAlgorithm);
+            
+            System.out.println("ForgedToken: " + forgedToken);
+            
             HttpHeaders headers = new HttpHeaders();
             headers.add("Authorization", "Bearer " + forgedToken);
             HttpEntity<String> request = new HttpEntity<String>(headers);
-
+            
             int offset = 0, limit = 4;
-
+            
             GlobalErrorResponse errorResponse = restTemplate.exchange(
                     "http://localhost:{port}/v1/products?offset={offset}&limit={limit}",
                     HttpMethod.GET, request, GlobalErrorResponse.class,
                     port, offset, limit).getBody();
-
+            
+            assertNotNull(errorResponse, "Expected an error response but got null");
             assertEquals(401, errorResponse.status());
             assertTrue(errorResponse.message().startsWith("The Token's Signature resulted invalid"));
             assertEquals("/v1/products", errorResponse.path());
         }
-
+        
         @Test
         @SneakyThrows
-        public void fail_and_return_formatted_response_on_wrong_crud_method () {
+        void fail_and_return_formatted_response_on_wrong_crud_method() {
             MockHttpServletRequestBuilder requestBuilder = get("/v1/auth/login")
                     .contentType(MediaType.APPLICATION_JSON);
-
+            
             String responseBody = mockMvc.perform(requestBuilder)
                     //.andDo(print()) // this is optional
                     .andExpect(status().is4xxClientError())
                     .andReturn().getResponse().getContentAsString();
-
+            
             GlobalErrorResponse errorResponse = objectMapper.readValue(responseBody, GlobalErrorResponse.class);
-
+            
             assertNotNull(errorResponse.timestamp());
             assertEquals(405, errorResponse.status());
             assertEquals("Request method 'GET' is not supported", errorResponse.message());
             assertEquals("/v1/auth/login", errorResponse.path());
         }
     }
-
+    
     @Nested
     class ValidationTests {
-
+        
         @Test
         @SneakyThrows
-        public void test_credentials_validation () {
+        void test_credentials_validation() {
             LoginRequest loginRequest = new LoginRequest("wrong-email", "pa$$word");
-
+            
             MockHttpServletRequestBuilder requestBuilder = post("/v1/auth/login")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(objectMapper.writeValueAsString(loginRequest));
-
+            
             String responseBody = mockMvc.perform(requestBuilder)
                     //.andDo(print()) // this is optional
                     .andExpect(status().is4xxClientError())
                     .andReturn().getResponse().getContentAsString();
-
+            
             GlobalErrorResponse errorResponse = objectMapper.readValue(responseBody, GlobalErrorResponse.class);
-
+            
             assertNotNull(errorResponse.data());
             assertEquals(2, ((Map<?, ?>) errorResponse.data()).size());
             assertNotNull(((Map<?, ?>) errorResponse.data()).get("email"));
