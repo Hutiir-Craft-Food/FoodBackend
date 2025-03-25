676fc205d9edc7998a6d4a22b2426fadd1ff3cf2
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2025-03-25 14:21:15 +0200

    SCRUM-142: Fixed LocalStorage API path issues on different OS (#73)

--- src/main/java/com/khutircraftubackend/storage/exception/InvalidArgumentException.java
+++ src/main/java/com/khutircraftubackend/storage/exception/InvalidArgumentException.java
@@ -1,9 +1,11 @@
 package com.khutircraftubackend.storage.exception;
 
-import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class InvalidArgumentException extends IOException {
-	public InvalidArgumentException(String message) {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidArgumentException extends RuntimeException {
+    public InvalidArgumentException(String message) {
         super(message);
     }
 }
