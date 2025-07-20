3f4c46cc85b98560617654560b97b3669e10e1f8
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2025-07-20 21:52:16 +0300

    SCRUM-201 replace @Pattern with @Email for email field and remove @Pattern from password in RegisterRequest (#90)

--- src/main/java/com/khutircraftubackend/auth/request/RegisterRequest.java
+++ src/main/java/com/khutircraftubackend/auth/request/RegisterRequest.java
@@ -1,22 +1,25 @@
 package com.khutircraftubackend.auth.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.khutircraftubackend.user.Role;
 import com.khutircraftubackend.seller.SellerDTO;
+import com.khutircraftubackend.user.Role;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
+        
+        @Email(message = "Неправильний формат e-mail")
         @NotBlank(message = "E-mail не може бути порожнім")
-        @Pattern(regexp = "^[A-Za-z\\d+_.-]+@[A-Za-z\\d.-]+\\.[A-Za-z]{2,}$")
         String email,
 
         @NotBlank(message = "Пароль не може бути порожнім")
-        @Size(min = 8, max = 30, message = "Пароль має містити від 8 до 30 символів")
-        @Pattern(regexp = "^[^!@#$%^&*()_+=]*$", message = "Пароль не може містити спеціальні символи !@#$%^&*()_+=")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=])[ A-Za-z\\d!@#$%^&*()_+\\-=]{8,30}$",
+                message = "Пароль має містити 8–30 символів, великі й малі латинські літери, цифри та спецсимволи і може містити пробіли"
+        )
         String password,
 
         @NotNull(message = "Роль не може бути порожньою.")
