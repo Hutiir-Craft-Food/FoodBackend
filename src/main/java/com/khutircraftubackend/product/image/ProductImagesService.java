168fb47f1a76ed6c2f8a83b440b20eb6eb0c6fe4
Ne4upara <ne4upara84@gmail.com>
2025-07-25 12:37:21 +0300

    add Tests

--- src/main/java/com/khutircraftubackend/product/image/ProductImagesService.java
+++ src/main/java/com/khutircraftubackend/product/image/ProductImagesService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
