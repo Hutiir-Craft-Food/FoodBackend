9242398206d1d656b86706affb0555ef4f333415
Yrii <caravell@ukr.net>
2025-09-20 10:09:42 +0300

    Fix LazyInitializationException in getProducts by adding @Transactional and batching prices

--- src/main/java/com/khutircraftubackend/product/ProductRepository.java
+++ src/main/java/com/khutircraftubackend/product/ProductRepository.java
@@ -3,6 +3,7 @@ package com.khutircraftubackend.product;
 import com.khutircraftubackend.seller.SellerEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     
+    @EntityGraph(attributePaths = {"seller", "category"})
     Page<ProductEntity> findAllBy(Pageable pageable);
     
     Optional<ProductEntity> findProductById(Long id);
