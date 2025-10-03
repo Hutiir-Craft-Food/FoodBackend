9d13b6416cbfd390c459bb213f9af86dc85786c8
Yrii <caravell@ukr.net>
2025-10-03 10:19:26 +0300

    SCRUM-236: Fix Auditable updatedAt conflict with SQL default by relying on Spring Data Auditing

--- src/main/resources/db/migration/V12__Create_product_units_table.sql
+++ src/main/resources/db/migration/V12__Create_product_units_table.sql
@@ -3,7 +3,7 @@ create table if not exists product_units
     id         bigint generated always as identity primary key,
     name       varchar(10) not null unique,
     created_at timestamptz not null default now(),
-    updated_at timestamptz not null default now()
+    updated_at timestamptz
 );
 
 create unique index if not exists product_units_name_uindex on product_units (name);
