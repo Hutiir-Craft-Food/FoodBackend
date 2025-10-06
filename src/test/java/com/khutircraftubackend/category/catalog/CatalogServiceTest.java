84d4fe19aded3fcc0944411031903632da0e2e58
P.F. <pavel.filkovskiy@gmail.com>
2025-10-06 16:10:07 +0100

    pavlo/SCRUM-216: rename PathServiceTest to CatalogServiceTest and refactor

---
+++ src/test/java/com/khutircraftubackend/category/catalog/CatalogServiceTest.java
@@ -0,0 +1,125 @@
+package com.khutircraftubackend.category.catalog;
+
+import com.khutircraftubackend.category.CategoryEntity;
+import com.khutircraftubackend.category.CategoryRepository;
+import com.khutircraftubackend.category.catalog.response.CategoryTreeNode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT) // TODO: what causes unnecessary stubbing warnings ?
+class CatalogServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private CategoryViewRepository categoryViewRepository;
+
+    @InjectMocks
+    private CatalogService catalogService;
+
+    @Test
+    @DisplayName("Should return full breadcrumbs tree when category exists")
+    void shouldReturnBreadcrumbs_WhenCategoryExists() {
+
+        // Arrange
+        CategoryViewEntity viewEntity = mock(CategoryViewEntity.class);
+        when(viewEntity.getJsonTree()).thenReturn("""
+            {
+                \"id\": 1, 
+                \"name\": \"Напої\", 
+                \"children\": [
+                    {
+                        \"id\": 11, 
+                        \"name\": \"Алкогольні\", 
+                        \"children\": [
+                            {
+                                \"id\": 111, 
+                                \"name\": \"Вина\", 
+                                \"children\": []
+                            }
+                        ]
+                    }
+                ]
+            }
+        """);
+
+        when(categoryViewRepository.findById(111L)).thenReturn(Optional.of(viewEntity));
+
+        //Act
+        CategoryTreeNode node = catalogService.getCatalogTree(111L);
+
+        //Assert
+        assertEquals("Напої", node.getName());
+        assertEquals(1, node.getChildren().size());
+
+        CategoryTreeNode alk = node.getChildren().get(0);
+        assertEquals("Алкогольні", alk.getName());
+
+        CategoryTreeNode vino = alk.getChildren().get(0);
+        assertEquals("Вина", vino.getName());
+    }
+
+    @Test
+    @DisplayName("Should return full catalog")
+    void shouldReturnEmpty_WhenCategoryMissing() {
+
+        // Arrange each time for all:
+        CategoryEntity entity1 = CategoryEntity.builder()
+                .id(1L)
+                .name("Напої")
+                .parentCategory(null)
+                .build();
+
+            CategoryEntity entity11 = CategoryEntity.builder()
+                    .id(11L)
+                    .name("Алкогольні")
+                    .parentCategory(entity1)
+                    .build();
+
+                CategoryEntity entity111 = CategoryEntity.builder()
+                        .id(111L)
+                        .name("Вина")
+                        .parentCategory(entity11)
+                        .build();
+
+            CategoryEntity entity12 = CategoryEntity.builder()
+                    .id(11L)
+                    .name("Безалкогольні")
+                    .parentCategory(entity1)
+                    .build();
+
+        CategoryEntity entity2 = CategoryEntity.builder()
+                .id(2L)
+                .name("Мʼясні вироби")
+                .parentCategory(null)
+                .build();
+
+        when(categoryRepository.findAll()).thenReturn(List.of(entity1, entity11, entity111, entity12, entity2));
+        when(categoryRepository.findAllByParentCategoryIsNull()).thenReturn(List.of(entity1, entity2));
+        when(categoryRepository.findAllByParentCategory_Id(1L)).thenReturn(List.of(entity11, entity12));
+        when(categoryRepository.findAllByParentCategory_Id(11L)).thenReturn(List.of(entity111));
+
+        //Act
+        List<CategoryTreeNode> result = catalogService.getCatalogTree();
+
+        //Assert
+        assertEquals(2, result.size());
+
+        // TODO: other assertions here....
+    }
+}
