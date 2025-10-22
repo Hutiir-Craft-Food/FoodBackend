d74f92c0581665e09944cd897bdc423e759622e6
P.F. <pavel.filkovskiy@gmail.com>
2025-10-22 21:16:04 +0100

    pavlo/SCRUM-176: refactor separate sql scrips into admin creation and seed db data

--- src/main/resources/db/migration/V7__Create_User_Admin.sql
+++ src/main/resources/db/migration/V7__Create_User_Admin.sql
@@ -3,3 +3,6 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO users (email, password, role, enabled, confirmed)
 VALUES
     ('${email}', crypt('${password}', gen_salt('bf')), '${role}', ${enabled}, ${confirmed});
+    -- TODO: what's gonna happen env variables are not set?
+    --  can we check that somehow at migration time?
+    --  can we fail the migration if they are not set?
