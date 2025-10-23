cce19758fc467e4065030c621a092f0827dabfdf
P.F. <pavel.filkovskiy@gmail.com>
2025-10-23 11:50:05 +0100

    SCRUM-176: revert V7__Create_User_Admin.sql

--- src/main/resources/db/migration/V7__Create_User_Admin.sql
+++ src/main/resources/db/migration/V7__Create_User_Admin.sql
@@ -1,5 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+-- INSERT INTO users (email, password, role, enabled, confirmed)
+-- VALUES ('${email}', crypt('${password}', gen_salt('bf')), '${role}', ${enabled}, ${confirmed});
+
 INSERT INTO users (email, password, role, enabled, confirmed)
-VALUES
-    ('${email}', crypt('${password}', gen_salt('bf')), '${role}', ${enabled}, ${confirmed});
+VALUES ('admin', crypt('admin', gen_salt('bf')), 'ADMIN', true, true);
