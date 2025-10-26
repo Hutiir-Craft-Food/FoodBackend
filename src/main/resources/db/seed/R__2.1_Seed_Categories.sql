f40e64590e53b7da95ace3c7c88a5cb984ceb66f
Ne4upara <ne4upara84@gmail.com>
2025-10-26 14:50:24 +0200

    SCRUM-251 add test data

---
+++ src/main/resources/db/seed/R__2.1_Seed_Categories.sql
@@ -0,0 +1,50 @@
+truncate table categories cascade;
+
+-- Top category
+INSERT INTO categories (name, description, parent_id)
+VALUES
+      ('Алкоголь', 'Горілка, вино, пиво', null),
+      ('М''ясо', 'Свіже м''ясо та м''ясні вироби', null),
+      ('Молочні продукти', 'Молоко, сири, йогурти та інше', null);
+
+  -- Subcategories under 'Алкоголь':
+INSERT INTO categories (name, description, parent_id)
+SELECT
+    sub.name,
+    sub.description,
+    c.id
+FROM (
+         VALUES
+             ('Пиво', 'Світле, темне,нефільтроване'),
+             ('Віскі', 'Для людей з витонченим смаком'),
+             ('Вино', 'Червоне, біле, рожеве вино')
+     ) AS sub(name, description)
+    JOIN  categories c ON c.name = 'Алкоголь' AND c.parent_id IS NULL;
+
+    -- Subcategories under 'М'ясо':
+INSERT INTO categories (name, description, parent_id)
+SELECT
+    sub.name,
+    sub.description,
+    c.id
+FROM (
+         VALUES
+             ('Свинина', 'Свіжа свинина'),
+             ('Яловичина', 'Висока якість яловичини'),
+             ('Курка', 'Куряче філе, стегна, крильця')
+     ) AS sub(name, description)
+        JOIN  categories c ON c.name = 'М''ясо' AND c.parent_id IS NULL;
+
+    -- Subcategories under 'Молочні продукти':
+INSERT INTO categories (name, description, parent_id)
+SELECT
+    sub.name,
+    sub.description,
+    c.id
+FROM (
+         VALUES
+             ('Сири', 'Різні види сирів'),
+             ('Молоко', 'Пастеризоване молоко'),
+             ('Йогурти', 'Натуральні та фруктові йогурти')
+     ) AS sub(name, description)
+        JOIN  categories c ON c.name = 'Молочні продукти' AND c.parent_id IS NULL;
---
+++ src/main/resources/db/seed/R__2.1_Seed_Product_Units.sql
@@ -0,0 +1,11 @@
+truncate table product_units cascade;
+
+INSERT INTO product_units (name)
+VALUES
+    ('мл.'),
+    ('л.'),
+    ('г.'),
+    ('кг.'),
+    ('уп.'),
+    ('бут.'),
+    ('шт.');
