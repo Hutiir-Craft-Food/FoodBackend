5a49f3dcc982fd704fb78fa4dba8aba4730f3b13
Yrii <caravell@ukr.net>
2025-10-27 16:32:22 +0200

    SCRUM-254 Fix image upload when category transaction rolls back
    SCRUM-254 Fix image upload when category transaction rolls back

--- src/main/java/com/khutircraftubackend/category/exception/CategoryExceptionMessages.java
+++ src/main/java/com/khutircraftubackend/category/exception/CategoryExceptionMessages.java
@@ -1,9 +1,14 @@
 package com.khutircraftubackend.category.exception;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class CategoryExceptionMessages{
     public static final String CATEGORY_NOT_FOUND = "Категорія не знайдена";
     public static final String CATEGORY_HAS_SUBCATEGORIES_OR_PRODUCTS =
             "Категорія має підкатегорії або продукти і не може бути видалена";
-
-    private CategoryExceptionMessages() {}
+    public static final String CATEGORY_ALREADY_EXISTS = "Категорія з такою назвою '%s' вже існує";
+    public static final String COULD_NOT_UPLOAD_ICON = "Не вдалося завантажити іконку категорії";
+    public static final String COULD_NOT_DELETE_ICON = "Не вдалося видалити іконку категорії з URL: ";
+    
 }
