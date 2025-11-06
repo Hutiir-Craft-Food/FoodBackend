fc1fdd410b138b5f64f9333c6dfbca891b6b2978
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2025-11-06 10:03:35 +0200

    SCRUM-262 Refactor prices and units in ProductResponse (#119)

--- src/main/java/com/khutircraftubackend/product/response/ProductResponse.java
+++ src/main/java/com/khutircraftubackend/product/response/ProductResponse.java
@@ -1,11 +1,14 @@
 package com.khutircraftubackend.product.response;
 
 import com.khutircraftubackend.category.response.CategoryResponse;
-import com.khutircraftubackend.product.price.response.ProductPriceResponse;
+import com.khutircraftubackend.product.price.entity.ProductUnitEntity;
+import com.khutircraftubackend.product.price.request.ProductPriceDTO;
 import com.khutircraftubackend.seller.SellerResponse;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record ProductResponse(
         
         Long id,
@@ -18,6 +21,7 @@ public record ProductResponse(
         String allergens,
         SellerResponse seller,
         CategoryResponse category,
-        List<ProductPriceResponse> prices
+        List<ProductPriceDTO> prices,
+        List<ProductUnitEntity> units
 ) {
 }
