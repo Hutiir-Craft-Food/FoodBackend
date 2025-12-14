1b40359ebddd74f647d65a426703bd15da5c44fc
Ne4upara <ne4upara84@gmail.com>
2025-12-14 10:06:09 +0200

    SCRUM-261

--- src/main/resources/db/migration/V14__Create_product_image_variants_table.sql
+++ src/main/resources/db/migration/V14__Create_product_image_variants_table.sql
@@ -1,13 +1,14 @@
+CREATE SEQUENCE product_image_variants_seq START WITH 1 INCREMENT BY 4;
+
 CREATE TABLE IF NOT EXISTS product_image_variants (
-    id BIGINT PRIMARY KEY,
+    id BIGINT PRIMARY KEY
+        DEFAULT nextval('product_image_variants_seq'),
     image_id BIGINT NOT NULL,
     link TEXT NOT NULL,
     ts_size VARCHAR NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ
     );
-
-CREATE SEQUENCE product_image_variants_seq START WITH 1 INCREMENT BY 4;
 
 CREATE INDEX idx_product_image_variants_image_id ON product_image_variants (image_id);
 CREATE INDEX idx_product_image_variants_size ON product_image_variants (ts_size);
