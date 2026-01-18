ba3ecbe674c4df838667b28a6fa96244157512f1
Yurii Ponomarenko <79604216+YP0n@users.noreply.github.com>
2026-01-18 11:12:22 +0200

    SCRUM-145 Auth: change login response to 401 for unregistered email (#130)

--- src/main/java/com/khutircraftubackend/auth/AuthenticationService.java
+++ src/main/java/com/khutircraftubackend/auth/AuthenticationService.java
@@ -14,6 +14,7 @@ import com.khutircraftubackend.seller.SellerService;
 import com.khutircraftubackend.user.UserEntity;
 import com.khutircraftubackend.user.UserRepository;
 import com.khutircraftubackend.user.UserService;
+import com.khutircraftubackend.user.exception.UserNotFoundException;
 import jakarta.persistence.PostUpdate;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -43,25 +44,29 @@ public class AuthenticationService {
     @Transactional
     public AuthResponse authenticate(LoginRequest request) {
         
-        UserEntity user = userService.findByEmail(request.email());
-        
         try {
+            UserEntity user = userService.findByEmail(request.email());
+            
             authenticateUser(request.email(), request.password());
+            
+            if (Boolean.FALSE.equals(user.isEnabled())) {
+                log.warn("Access denied for blocked user, email={}", request.email());
+                throw new UserBlockedException(AUTH_USER_BLOCKED);
+            }
+            
+            return AuthResponse.builder()
+                    .jwt(jwtUtils.generateJwtToken(user.getEmail()))
+                    .role(user.getRole())
+                    .confirmed(user.isConfirmed())
+                    .build();
+        } catch (UserNotFoundException ex) {
+            log.warn("Authentication failed: user not found, email={}", request.email());
+            throw new InvalidCredentialsException(AUTH_INVALID_CREDENTIALS);
+            
         } catch (BadCredentialsException ex) {
-            log.warn("Authentication failed: bad credentials, email={}", request.email());
+            log.warn("Authentication failed: wrong password, email={}", request.email());
             throw new InvalidCredentialsException(AUTH_INVALID_CREDENTIALS);
         }
-        
-        if (Boolean.FALSE.equals(user.isEnabled())) {
-            log.warn("Access denied for blocked user, email={}", request.email());
-            throw new UserBlockedException(AUTH_USER_BLOCKED);
-        }
-        
-        return AuthResponse.builder()
-                .jwt(jwtUtils.generateJwtToken(user.getEmail()))
-                .role(user.getRole())
-                .confirmed(user.isConfirmed())
-                .build();
     }
     
     private void authenticateUser(String email, String password) {
---
+++ src/main/java/com/khutircraftubackend/user/exception/UserResponseMessage.java
@@ -0,0 +1,9 @@
+package com.khutircraftubackend.user.exception;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserResponseMessage {
+    
+    public static final String USER_NOT_FOUND = "Користувача не знайдено";
+}
