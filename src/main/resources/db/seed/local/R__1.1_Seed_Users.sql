75314a5cda648089d1a60ac4a038756b017bd0b7
P.F. <pavel.filkovskiy@gmail.com>
2026-03-05 20:54:14 +0000

    SCRUM-313: add mvn plugin for flyway

--- src/main/resources/db/seed/local/R__1.1_Seed_Users.sql
+++ src/main/resources/db/seed/local/R__1.1_Seed_Users.sql
@@ -1,17 +1,23 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-  -- admin:
-  INSERT INTO users (email, password, role, enabled, confirmed)
-  SELECT 'admin@example.com', crypt('admin', gen_salt('bf')), 'ADMIN', true, true
-  WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@example.com');
+INSERT INTO users (email, password, role, enabled, confirmed)
+VALUES
+    ('admin@example.com', crypt('admin', gen_salt('bf')), 'ADMIN', true, true),
+    ('seller@example.com', crypt('!sellerTop1', gen_salt('bf')), 'SELLER', true, true),
+    ('buyer@example.com', crypt('!buyerTop1', gen_salt('bf')), 'BUYER', true, true)
+ON CONFLICT (email) DO NOTHING;
 
-  -- seller & buyer:
-  INSERT INTO users (email, password, role, enabled, confirmed)
-  VALUES
-      ('seller@example.com', crypt('!sellerTop1', gen_salt('bf')), 'SELLER', true, true),
-      ('buyer@example.com', crypt('!buyerTop1', gen_salt('bf')), 'BUYER', true, true);
-
-  -- Seller's profile:
-  INSERT INTO sellers (seller_name, user_id)
-  SELECT 'Seller' as seller_name, id as user_id
-  FROM users WHERE email = 'seller@example.com';
+INSERT INTO sellers (seller_name, user_id)
+SELECT 'Seller' as seller_name, id as user_id
+FROM users
+WHERE email = 'seller@example.com'
+AND NOT EXISTS (
+    SELECT 1
+    FROM sellers
+    WHERE user_id = users.id
+);
+-- the below cant be used.
+-- ON CONFLICT (user_id) DO NOTHING;
+-- coz it requires user_id to be unique
+-- but we might want to allow multiple Seller profiles
+-- under the same user accounts
